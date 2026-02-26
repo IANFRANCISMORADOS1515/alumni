27c996a0f498c6c6b6a299ed8aa20d8558b4e24f
Ian <ianfrancis100115@gmail.com>
2026-02-26 16:31:18 +0800

    Initial commit

---
+++ include/footer.php
@@ -0,0 +1,8 @@
+
+            <footer class="footer">
+                <div class="container-fluid d-flex justify-content-between">
+                    <div class="copyright">
+                        @ 2026 Sandigan Colleges Incorporated. All Rights Reserved.
+                    </div>
+                </div>
+            </footer>
