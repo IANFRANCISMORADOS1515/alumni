68f73e290cd94e9d836b496cc4cf1f59765c67f8
Ian <ianfrancis100115@gmail.com>
2026-03-09 09:56:16 +0800

    change folder

--- include/footer.php
+++
@@ -1,7 +0,0 @@
-           <footer class="footer">
-                <div class="container-fluid d-flex justify-content-between">
-                    <div class="copyright">
-                        @ 2026 Sandigan Colleges Incorporated. All Rights Reserved.
-                    </div>
-                </div>
-            </footer>
